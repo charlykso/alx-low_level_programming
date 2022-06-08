66818390ca81e5987c26fc3aa9b4af99d65fc7b7
charlykso <charlykso141@gmail.com>
2022-06-07 20:04:25 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - shows if an input is positive or negative
+ * @n: n is a parameter of the function
+ *
+ * Return: return 1 for +ve num, -1 for -ve num and 0 for zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
