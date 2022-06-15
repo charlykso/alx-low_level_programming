8d20b55855a10118e4652a8e7a034b804e387f15
charlykso <charlykso141@gmail.com>
2022-06-15 13:48:34 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,24 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - a finction that concatinates two strings
  * @dest: parameter for the function
  * @src: another parameter for the function
- * @n: this takes the number of character that will be
- * joined 
+ * @n: this takes the number of character that will be joined
  *
  * Return: Always 0
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x, i, len;
+	int i, lendest;
 
-	len = 0;
-	while (dest[len] != '\0')
-	{
-		len++;
-	}
-	x = len;
+	lendest = strlen(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[x + i] = src[i];
+		dest[lendest + i] = src[i];
 	}
-	dest[x + i] = '\0';
+	dest[lendest + i] = '\0';
 
 	return (dest);
 }
