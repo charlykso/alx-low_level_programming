ec027f4c552d157e3585ef2a5a20561ab0693d0b
charlykso <charlykso141@gmail.com>
2022-06-24 14:15:37 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-testing.c
+++ 0x0A-argc_argv/4-testing.c
@@ -11,19 +11,27 @@
  */
 int main(int argc, char **argv)
 {
-	int i, j, sum;
+	int i, sum;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	if (argc > 0)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (atoi(argv[i]) > 0)
 			{
-				return (puts("Error"), 1);
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-	return (printf("%d\n", sum), 0);
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
 }
