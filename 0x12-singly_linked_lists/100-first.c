da2d286df8a6deb9457e0d4122e35dd4a7de5e40
charlykso <charlykso141@gmail.com>
2022-07-16 01:41:38 +0000

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void myStartupFun (void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
-* myStartupFun - a function that prints before the main
+* first - a function that prints before the main
 *
 * Return: void
 */
 
-void myStartupFun(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("\nI bore my house upon my back!\n");
