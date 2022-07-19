5af459d173c918de944bb1fba7080d2fa9ca7a3b
IkennaRemigius <charlykso141@gmail.com>
2022-07-19 08:32:28 -0400

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,6 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
 	int i;
 
 	if (head == NULL)
@@ -18,8 +17,6 @@ int pop_listint(listint_t **head)
 		return (0);
 	}
 	i = (*head)->n;
-	temp = (*head)->next;
 	free(*head);
-	*head = temp;
 	return (i);
 }
