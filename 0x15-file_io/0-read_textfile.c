984fcccb1eafa715f8b809d59f77a19c0fccd35c
IkennaRemigius <charlykso141@gmail.com>
2022-07-25 17:02:54 -0400

    copy

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,19 +15,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 
 	if (!filename)
-	{
 		return (0);
-	}
+
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-	{
 		return (0);
-	}
+
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-	{
 		return (0);
-	}
+
 	s = read(fd, buf, letters);
 	if (s < 0)
 	{
@@ -35,7 +32,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	buf[s] = '\0';
+
 	close(fd);
+
 	t = write(STDOUT_FILENO, buf, s);
 	if (t < 0)
 	{
