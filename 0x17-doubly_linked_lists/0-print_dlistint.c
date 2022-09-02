aa13832c0e7646fac36ed77f50f9f5dd2e0b4d60
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-01 20:18:06 -0400

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that prints doubly linked list data
+ * @h: the head of the doubly linked list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t count = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d \n", temp->n);
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
