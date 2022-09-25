d0d63eb93f39080aa60fe305fb2f49fb4fd40584
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-25 15:22:22 -0400

    betty coding style corrections

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,9 +12,9 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
-	
+
 	if (size == 0)
-		return (NULL);
+		return (0);
 	index = hash_djb2(key);
 	return (index % size);
 }
