73ec2319671ac959576157175c91f9a2756e6985
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-04 00:25:25 -0500

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,64 @@
+#include "search.h"
+#include "stdio.h"
+void print_search(int *array, int first, int last);
+
+/**
+ * binary_search - function that searches for a
+ * value in a sorted array of integers using the BSA
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int start = 0;
+	unsigned int end = size - 1;
+	unsigned int middle = 0;
+
+	while (start <= end)
+	{
+		middle = (start + end) / 2;
+		print_search(array, start, end);
+		if (array[middle] == value)
+			return (middle);
+		if (array[middle] > value)
+			end = middle - 1;
+		else
+			start = middle + 1;
+	}
+	return (-1);
+}
+
+
+/**
+ * print_search - Prints arrayand side of array
+ *
+ * @array: Array to search
+ *
+ * @first: left hand side of array
+ *
+ * @last: right hand side of array
+ *
+ * Return: Void
+ */
+
+void print_search(int *array, int first, int last)
+{
+	int index = 0;
+
+	printf("Searching in array: ");
+
+	while (first <= last)
+	{
+		if (index > 0)
+		{
+			printf(", ");
+		}
+		index = first++;
+		printf("%d", array[index++]);
+	}
+	printf("\n");
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,11 @@
-#ifndef __LIST_H__
-#define __LIST_H__
+#ifndef __SEARCH_ALGO_H__
+#define __SEARCH_ALGO_H__
 
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
